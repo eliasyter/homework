0339a06ee8b8f1bcaab6512760df1687e6287bec
eliasyter <elias.yter@gmail.com>
2024-11-06 20:17:09 +0100

    adding subjects

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ from wtforms.validators import InputRequired, Email,length, NumberRange
 class Lekse_form(FlaskForm):
     
     frist=DateField('frist',validators=[InputRequired()])
-    fag=SelectField('fag',coerce=str, choices=[('matte'),('Fysikk')])
+    fag=SelectField('fag',coerce=str, choices=[('Chimie'),('Mathématiques 0'), ('Mathématiques 1'), ('PPI'), ('Mécanique'),('Optique'),('FLE'),('Sciences industrielles')])
     oppgave = StringField('oppgave', validators=[InputRequired(),length(max=120)],render_kw={"placeholder": "Oppgave 2 kapitel 3 "})
     
 
